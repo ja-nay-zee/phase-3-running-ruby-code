bf269aa9abf8881ba57d38f147abba93d2258c96
Yaneizy Castillo <yccastillo18@gmail.com>
2021-09-19 00:08:17 -0400

    Completed assignment

---
+++ app.rb
@@ -0,0 +1,13 @@
+puts "Hello World!"
+# # puts "Hello world!"
+# # puts "Hello world!"
+# # puts [1,2,3]
+
+
+print "Pass this test, please."
+# # print "Hello!"
+# # print "Hello!"
+# # p [1,2,3]
+p [1,2,3]
+
+# # pp [{ id: 1, hello: "World", name: "Yaneizy" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
